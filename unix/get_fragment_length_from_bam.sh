a20648c6979bd77fc4896b89105cb86511c8bf28
crazyhottommy <tangming2005@gmail.com>
2019-02-23 00:08:07 -0500

    raw data

--- unix/get_fragment_length_from_bam.sh
+++ unix/get_fragment_length_from_bam.sh
@@ -8,13 +8,11 @@ set -o pipefail
 ## fragment length <=30 are artifact
 ## see https://github.com/GreenleafLab/NucleoATAC/issues/18
 ## the shell script was adopted from Xi Chen https://dbrg77.wordpress.com/2017/02/10/atac-seq-insert-size-plotting/
-bam=$1
-out=$2
-
 show_help() {
 cat << EOF
-get the counts of fragment length from a bam file
-The first column is the count of the fragment length in the second column
+get the fragment length of every read pair from a bam file
+The output can be plotted to identify the ~200 bp peak 
+see https://github.com/GreenleafLab/NucleoATAC/issues/18
 
 Usage: ${0##*/}  <path to bam file> <output txt file>
 Example: ${0##*/} my.bam my.txt
@@ -22,13 +20,15 @@ EOF
 }
 
 if [[ $# -le 1 ]];then show_help;exit 1;fi
-# read input
 
 ## check if samtools in path
 
 ((command -v samtools) > /dev/null) || \
 (echo "[get_fragment_length_from_bam.sh] error: samtools is not in your $PATH, \
 	install it by 'conda install -c bioconda samtools' on your machine" && exit 1)
+
+bam=$1
+out=$2
 
 check_file_exists() {
     if [ ! -f "${bam}" ]; then
@@ -39,6 +39,10 @@ check_file_exists() {
 
 check_file_exists
 
+#samtools view $bam | awk '$9>0' | cut -f 9 | sort | uniq -c | sort -b -k2,2n \
+#| sed -e 's/^[ \t]*//' | awk '{print $1"\t"$2}' > $out
 
-samtools view $bam | awk '$9>0' | cut -f 9 | sort | uniq -c | sort -b -k2,2n \
-| sed -e 's/^[ \t]*//' | awk '{print $1"\t"$2}' > $out
+# this is better for downstream R plotting using the raw data
+samtools view $bam | awk '$9>0' | cut -f 9 > $out
+
+
